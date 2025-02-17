a58e415a5bcea504aa1e184a5af84f2e5870599a
CTZNpk <haidersultan7675@gmail.com>
2025-02-17 16:49:47 +0500

    Configure MongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const port = 3000;
+
+const mongoUrl = process.env.MONGODB_URL!;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error("Failed to Connect to MongoDB", err));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
